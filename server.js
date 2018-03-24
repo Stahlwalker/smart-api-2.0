5f78d287ece267146241a46a8710e7ee93d74a19
Luke Stahl <Stahl1109@gmail.com>
2018-03-24 18:51:59 -0500

    no knew error wtf

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image= require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-dimensional-75754',
-      user : 'lstahl',
-      password : '',
-      database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
     }
   });
 

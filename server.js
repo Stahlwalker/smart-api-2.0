050beb5f11cbfad2110dd6a57cd6e8213fbdeee3
Luke Stahl <Stahl1109@gmail.com>
2018-03-24 18:12:46 -0500

    start over commit

--- server.js
+++ server.js
@@ -12,18 +12,53 @@ const image= require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
+      host : '127.0.0.1',
+      user : 'lstahl',
+      password : '',
+      database : 'smart-brain'
     }
   });
-  console.log(process.env.DATABASE_URL);
+
+// db.select('*'). from('users').then(data => {
+//     // console.log(data);
+// });
+
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {res.send('it is working!') })
+
+// const database = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'John',
+//             password: 'cookies',
+//             email: 'john@gmail.com',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '124',
+//             name: 'Sally',
+//             password: 'bananas',
+//             email: 'sally@gmail.com',
+//             entries: 0,
+//             joined: new Date()
+//         }
+//     ],
+//     login: [
+//       {  
+//         id: '987',
+//         has: '',
+//         email: 'john@gmail.com'
+//       }
+//     ]
+// }
+
+app.get('/', (req, res) => {res.send(database.users) })
 //alt way of writing see signin post and sigin.js
 // app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt )})
 app.post('/signin', signin.handleSignin(db, bcrypt))
@@ -32,6 +67,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db )}
 app.put('/image', (req, res) => { image.handleImage(req, res, db )})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res )})
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`app is running on port ${process.env.PORT}`)
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000')
 })
